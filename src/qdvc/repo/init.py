d9d17fff9e22f640c31f44f4421fa3f9b0f61806
Jenkins Automation <guillaume.mosching@gmail.com>
2022-08-10 18:22:46 +0200

    Adding new init files on git

--- src/qdvc/repo/init.py
+++ src/qdvc/repo/init.py
@@ -1,6 +1,7 @@
 from dvc.repo import Repo as DvcRepo
-from qdvc.config import Config
+from git.repo import Repo as GitRepo
 from qdvc.repo import Repo
+from qdvc.config import Config
 from dvc.utils import relpath
 from dvc.utils.fs import remove
 from dvc.exceptions import InitError
@@ -26,16 +27,27 @@ def init(root_dir=os.curdir, no_scm=False, force=False, subdir=False):
     DvcRepo.init(root_dir, no_scm, force, subdir)
 
     root_dir = os.path.realpath(root_dir)
-    qdvc_dir = os.path.join(root_dir, Repo.QDVC_DIR)
-
-    if os.path.isdir(qdvc_dir):
-        if not force:
-            raise InitError(f"'{relpath(qdvc_dir)}' exists. Use `-f` to force.")
-
-        remove(qdvc_dir)
-
-    os.mkdir(qdvc_dir)
+    qdvc_dir = _create_dir(root_dir, Repo.QDVC_DIR, force)
+    data_dir = _create_dir(root_dir, Repo.QDVC_DATA_DIR, force)
 
     _ = Config.init(qdvc_dir)
 
+    repo = GitRepo(root_dir)
+
+    repo.index.add(data_dir)
+    repo.index.add(qdvc_dir)
+    # repo.git_repo.index.commit("Initial QDVC commit")
+
     return DvcRepo(root_dir)
+
+
+def _create_dir(rootdir: str, sudir: str, force: bool) -> str:
+    new_dir = os.path.join(rootdir, sudir)
+
+    if os.path.isdir(new_dir):
+        if not force:
+            raise InitError(f"'{relpath(new_dir)}' exists. Use `-f` to force.")
+        remove(new_dir)
+
+    os.mkdir(new_dir)
+    return new_dir
